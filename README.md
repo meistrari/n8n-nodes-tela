c3ece6e429f64b8ff2e911e01b92febe192d5a54
Renan Medeiros <renanllm01@gmail.com>
2025-08-18 17:20:46 -0300

    Update version to 1.0.14, enhance authentication method in Tela.credentials.ts, and update changelog in README.md

--- README.md
+++ README.md
@@ -266,7 +266,7 @@ npm run build && npm test
 
 ## Changelog
 
-### Version 1.0.9
+### Version 1.0.14
 
 - Added credential test method to fix n8n verification pre-checks
 - Fixed n8n verification pre-checks by restructuring project to match n8n-nodes-starter template
--- credentials/Tela.credentials.ts
+++ credentials/Tela.credentials.ts
@@ -1,4 +1,4 @@
-import { IconFile, ICredentialType, INodeProperties, ICredentialTestRequest } from "n8n-workflow";
+import { IconFile, ICredentialType, INodeProperties, ICredentialTestRequest, IAuthenticateGeneric } from "n8n-workflow";
 
 export class Tela implements ICredentialType {
   name = "telaApi";
@@ -12,19 +12,43 @@ export class Tela implements ICredentialType {
       type: "string",
       default: "",
       description: "Your Tela API key. You can find this in your Tela dashboard.",
+      typeOptions: { password: true },
       required: true,
     },
   ];
+
+  // test: ICredentialTestRequest = {
+  // 		request: {
+  // 			baseURL: 'https://api.tavily.com',
+  // 			url: '/search',
+  // 			method: 'POST',
+  // 			headers: {
+  // 				Authorization: '=Bearer {{$credentials.apiKey}}',
+  // 				'X-Client-Source': 'n8n'
+  // 			},
+  // 			body: {
+  // 				query: 'Hello n8n!',
+  // 			},
+  // 		},
+  // 	};
+
+  authenticate: IAuthenticateGeneric = {
+    type: 'generic',
+    properties: {
+      headers: {
+        Authorization: '=Bearer {{$credentials.apiKey}}',
+      },
+    },
+  };
 
   // The block below tells how this credential can be tested
   test: ICredentialTestRequest = {
     request: {
       baseURL: 'https://api.tela.com',
-      url: '/project',
+      url: '/api-key',
       method: 'GET',
       headers: {
-        'Authorization': 'Bearer {{ $credentials.apiKey }}',
-        'Content-Type': 'application/json',
+        Authorization: '=Bearer {{$credentials.apiKey}}',
       },
     },
   };
